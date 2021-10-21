57c87af9363bc51cb4a93ba26ea82e2ec059d1f3
Cédric RENAIS <cedric.renais@gmail.com>
2021-10-21 16:46:31 +0200

    v0.007 : Modification of the product.js file.
    addition of more data saved in the localStorage.

--- front/js/product.js
+++ front/js/product.js
@@ -3,103 +3,105 @@
 //-------------------------------------//
 
 const newID = new URLSearchParams(location.search);
-const productID = newID.get('_id');
+const dataID = newID.get('_id');
 
-//-----------------------------------------------------------------//
-// call the product ID in the API                                  //
-// transform data into json format                                 //
-// if the API does not respond, then an error message is displayed //
-//-----------------------------------------------------------------//
+//-------------------------------------------------------------------------------//
+// call the product ID in the API                                                //
+// transform data into json format                                               //
+// declaration of variables to retrieve locations to display elements in the DOM //
+// display elements in the DOM                                                   //
+// for loop of which look in the array for the color options then display it     //
+//-------------------------------------------------------------------------------//
 
-fetch(`http://localhost:3000/api/products/${productID}`)
-  .then((response) => response.json())
-  .then((data) => {
-    addProducts(data);
-    console.log(data);
+fetch(`http://localhost:3000/api/products/${dataID}`)
+  .then((response) => {
+    return response.json();
   })
+  .then((data) => {
+    const image = document.querySelector(
+      'body > main > div > section > article > div.item__img'
+    );
+    const description = document.getElementById('description');
+    const price = document.getElementById('price');
+    const name = document.getElementById('title');
+    const colors = document.getElementById('colors');
+    image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+    name.innerHTML = `<h1>${data.name}</h1>`;
+    price.innerText = `${data.price}`;
+    description.innerText = `${data.description}`;
+    for (index in data.colors) {
+      colors.options[colors.options.length] = new Option(
+        data.colors[index],
+        data.colors[index]
+      );
+    }
+
+    //-------------------------------------------------------------------------------------//
+    // add an event when clicking on the addToCart ID                                      //
+    // create an array containing the data of the product                                  //
+    // if the localStorage is not empty                                                    //
+    // look for the product in the array                                                   //
+    // if the product is not in the array then its value is -1                             //
+    // if the product is in the array then add 1 to the quantity                           //
+    // if localStorage is empty then create an array                                       //
+    //-------------------------------------------------------------------------------------//
+
+    const addToCart = document.getElementById('addToCart');
+    addToCart.addEventListener('click', (event) => {
+      event.preventDefault();
+      const dataQuantity = document.getElementById('quantity');
+      const dataColor = document.getElementById('colors');
+
+      const dataArray = {
+        id: dataID,
+        name: data.name,
+        price: data.price,
+        color: dataColor.value,
+        quantity: dataQuantity.value,
+        image: data.imageUrl,
+        alt: data.altTxt,
+      };
+
+      if (
+        localStorage.getItem('localStorageProducts') &&
+        localStorage.getItem('localStorageProducts').length > 0
+      ) {
+        const localStorageProducts = JSON.parse(
+          localStorage.getItem('localStorageProducts')
+        );
+        const data = localStorageProducts.findIndex(
+          (data) => data.id === dataArray.id && data.color === dataArray.color
+        );
+        if (data === -1) {
+          localStorageProducts.push(dataArray);
+          localStorage.setItem(
+            'localStorageProducts',
+            JSON.stringify(localStorageProducts)
+          );
+        } else {
+          localStorageProducts[data].quantity =
+            parseInt(localStorageProducts[data].quantity) +
+            parseInt(dataArray.quantity);
+          localStorage.setItem(
+            'localStorageProducts',
+            JSON.stringify(localStorageProducts)
+          );
+        }
+      } else {
+        localStorageProducts = [];
+        localStorageProducts.push(dataArray);
+        localStorage.setItem(
+          'localStorageProducts',
+          JSON.stringify(localStorageProducts)
+        );
+      }
+    });
+  })
+
+  //-----------------------------------------------------------------//
+  // if the API does not respond, then an error message is displayed //
+  //-----------------------------------------------------------------//
+
   .catch((error) => {
     alert('Notre serveur de répond pas, veuillez revenir ultérieurement.');
   });
-
-//-------------------------------------------------------------------------------------------------------------//
-// creation of a function to display the product                                                               //
-// declaration of variables to retrieve locations to display elements in the DOM                               //
-// display elements in the DOM                                                                                 //
-// for loop of which look in the array for the color options then display it                                   //
-//-------------------------------------------------------------------------------------------------------------//
-
-function addProducts(product) {
-  const productImg = document.querySelector(
-    'body > main > div > section > article > div.item__img'
-  );
-  const productDescription = document.getElementById('description');
-  const productPrice = document.getElementById('price');
-  const productName = document.getElementById('title');
-  const productColors = document.getElementById('colors');
-  productImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
-  productName.innerHTML = `<h1>${product.name}</h1>`;
-  productPrice.innerText = `${product.price}`;
-  productDescription.innerText = `${product.description}`;
-  for (index in product.colors) {
-    productColors.options[productColors.options.length] = new Option(
-      product.colors[index],
-      product.colors[index]
-    );
-  }
-}
-
-//-------------------------------------------------------------------------------------//
-// add an event when clicking on the addToCart ID                                      //
-// declaration of variables to retrieve locations in the DOM                           //
-// create an array containing the ID, the color option and the quantity of the product //
-// if the localStorage is not empty                                                    //
-// look for the product in the array                                                   //
-// if the product is not in the array then its value is -1                             //
-// if the product is in the array then add 1 to the quantity                           //
-// if localStorage is empty then create an array                                       //
-//-------------------------------------------------------------------------------------//
-
-document.getElementById('addToCart').addEventListener('click', (event) => {
-  event.defaultPrevented;
-  const productColor = document.getElementById('colors');
-  const productQuantity = document.getElementById('quantity');
-  const productArray = {
-    id: productID,
-    color: productColor.value,
-    quantity: productQuantity.value,
-  };
-  if (
-    localStorage.getItem('localStorageProducts') &&
-    localStorage.getItem('localStorageProducts').length > 0
-  ) {
-    const localStorageProducts = JSON.parse(
-      localStorage.getItem('localStorageProducts')
-    );
-    const product = localStorageProducts.findIndex(
-      (product) =>
-        product.id === productArray.id && product.color === productArray.color
-    );
-    if (product === -1) {
-      localStorageProducts.push(productArray);
-      localStorage.setItem(
-        'localStorageProducts',
-        JSON.stringify(localStorageProducts)
-      );
-    } else {
-      localStorageProducts[product].quantity =
-        parseInt(localStorageProducts[product].quantity) +
-        parseInt(productArray.quantity);
-      localStorage.setItem(
-        'localStorageProducts',
-        JSON.stringify(localStorageProducts)
-      );
-    }
-  } else {
-    localStorageProducts = [];
-    localStorageProducts.push(productArray);
-    localStorage.setItem(
-      'localStorageProducts',
-      JSON.stringify(localStorageProducts)
-    );
-  }
-});
