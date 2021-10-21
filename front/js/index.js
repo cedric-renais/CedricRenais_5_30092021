7b4d0219d4efef15fb0d051120bbb76ff2c0a4cf
Cédric RENAIS <cedric.renais@gmail.com>
2021-10-21 08:07:29 +0200

    v0.002 : Improvement of the codebase of the index.js file.
    refactoring of the fetch request |refactoring of the thumbnail display function.

--- front/js/index.js
+++ front/js/index.js
@@ -1,47 +1,37 @@
-//------------------------------------------//
-// declaration of the API url as a variable //
-//------------------------------------------//
+//-----------------------------------------------------------------//
+// API call                                                        //
+// transform data into json format                                 //
+// if the API does not respond, then an error message is displayed //
+//-----------------------------------------------------------------//
+fetch('http://localhost:3000/api/products')
+  .then((response) => response.json())
+  .then((data) => {
+    addProduct(data);
+  })
+  .catch((error) => {
+    alert('Notre serveur de répond pas, veuillez revenir ultérieurement.');
+  });
 
-const url = 'http://localhost:3000/api/products';
-
-//--------------------------------//
-// retrieve the data from the API //
-//--------------------------------//
-
-// creation of the function
-// API call
-// transform data into json format
-// display the list of data in the console
-// loop which iterates through the array and creates a variable for each element
-// declare a variable containing the HTML to display
-// parses the specified text as HTML and inserts the resulting nodes into the DOM tree at a specified position
-// avoids the extra step of serialization, making it much faster than direct innerHTML manipulation
-
-function getProducts() {
-  fetch(url).then((data) =>
-    data.json().then((data) => {
-      console.log(data);
-      for (index = 0; index < data.length; index++) {
-        const imageUrl = data[index].imageUrl;
-        const altTxt = data[index].altTxt;
-        const name = data[index].name;
-        const id = data[index]._id;
-        const description = data[index].description;
-        const price = data[index].price;
-        const thumbnails = `<a href="../html/product.html?id=${id}">
-        <article>
-        <img src="${imageUrl}" alt="${altTxt}">
-        <h3 class="productName">${name}</h3>
-        <p class="productDescription">${description}</p>
-        <p>Prix : ${price} €</p>
-        </article>
-        </a> `;
-        document
-          .getElementById('items')
-          .insertAdjacentHTML('beforeend', thumbnails);
-      }
-    })
-  );
+//-------------------------------------------------------------------------------------------------------------//
+// creation of a function for the display of articles                                                          //
+// for of loop which iterates through the array and creates a variable for each element                        //
+// parses the specified text as HTML and inserts the resulting nodes into the DOM tree at a specified position //
+// avoids the extra step of serialization, making it much faster than direct innerHTML manipulation            //
+//-------------------------------------------------------------------------------------------------------------//
+function addProduct(data) {
+  for (product of data) {
+    const thumbnails = `
+          <a href="./product.html?_id=${product._id}">
+          <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+             <h3 class="productName">${product.name}</h3>
+             <p class="productDescription">${product.description}</p>
+          </article>
+          </a>
+        `;
+    document
+      .getElementById('items')
+      .insertAdjacentHTML('beforeend', thumbnails);
+    console.log(product);
+  }
 }
-
-getProducts();
